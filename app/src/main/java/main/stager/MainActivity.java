84b9a4a167c406ddcf0d815441437dac79294bb0
Бабышев Максим <cmdazaza@gmail.com>
2021-03-15 12:42:21 +0300

    + Add Settings transition button

--- app/src/main/java/main/stager/MainActivity.java
+++ app/src/main/java/main/stager/MainActivity.java
@@ -1,7 +1,11 @@
 package main.stager;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +15,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Прикрепляем значок настроек на панель инструментов
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.main_activity_actions, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                Settings.openSettings(this);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+
 }
--- app/src/main/java/main/stager/Settings.java
+++ app/src/main/java/main/stager/Settings.java
@@ -1,7 +1,12 @@
 package main.stager;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import java.util.Set;
 
 public class Settings extends AppCompatActivity {
     // SETTINGS name
@@ -13,10 +18,50 @@ public class Settings extends AppCompatActivity {
     public static final String THEME_LIGHT = "__theme_light__";
     public static final String THEME_DEFAULT = THEME_LIGHT;
 
+    // Hidden
+    private static final String _PARENT = "__PARENT_ACTIVITY_CLASS__";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         ThemeController.restoreTheme(this);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+    }
+
+    /** Открывает окно настроек
+     * @param activity куда вернуться
+     */
+    public static void openSettings(Activity activity) {
+        Intent intent = new Intent(activity, Settings.class);
+        intent.putExtra(_PARENT, Settings.class.getName());
+        activity.startActivity(intent);
+    }
+
+    @Nullable
+    @Override
+    public Intent getParentActivityIntent() {
+        return _getParentActivityIntent();
+    }
+
+    @Nullable
+    @Override
+    public Intent getSupportParentActivityIntent() {
+        // Требуется для работы на старых версиях Android
+        return _getParentActivityIntent();
+    }
+
+    private Intent _getParentActivityIntent() {
+        // Получение активности, вызвавшей данную
+        Intent intent = null;
+        try {
+            intent = new Intent(this,
+                    Class.forName(getIntent()
+                                 .getExtras()
+                                 .getString(_PARENT))
+            );
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return intent;
     }
 }

e7184ec51599640aef69bf1b00a131bb7aa5f3ce
Dedicated407 <ilyaboy@list.ru>
2021-03-29 17:14:44 +0300

    + Catch exceptions in registration

--- app/src/main/java/main/stager/Registration.java
+++ app/src/main/java/main/stager/Registration.java
@@ -1,7 +1,10 @@
 package main.stager;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -15,10 +18,17 @@ import com.google.firebase.auth.FirebaseAuth;
 public class Registration extends SmartActivity {
     private EditText edNickname, edEmail, edPassword, edPasswordCheck;
     private FirebaseAuth fbAuth;
-    private Button btnSignIn, btnContinue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        fbAuth = FirebaseAuth.getInstance();
+
+        // Проверка на то что пользователь не авторизован
+        if (fbAuth.getCurrentUser() != null) {
+            startActivity(new Intent(this, MainActivity.class));
+            finish();
+        }
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.registr_form);
         init();
@@ -30,52 +40,110 @@ public class Registration extends SmartActivity {
         edPassword = findViewById(R.id.password);
         edPasswordCheck = findViewById(R.id.check_password);
 
-        fbAuth = FirebaseAuth.getInstance();
+        // Регистрация аккаунта
+        findViewById(R.id.success_btn).setOnClickListener(
+                v -> onClickRegistration()
+        );
 
-        onClickRegistration();
-        onClickSignIn();
-    }
-
-    private void onClickRegistration() {
-        btnContinue = (Button) findViewById(R.id.success_btn);
-//        String id = fbAuth.getAccessToken();
-
-        btnContinue.setOnClickListener(
-                v -> {
-                    String id = fbAuth.getUid();
-                    String name = edNickname.getText().toString();
-                    String email = edEmail.getText().toString();
-                    String password = edPassword.getText().toString();
-                    String passwordCheck = edPasswordCheck.getText().toString();
-
-                    if (password.equals(passwordCheck) && !name.isEmpty() && !email.isEmpty()) {
-//                        User user = new User(id, name, email, password);
-//                        fbAuth.sendSignInLinkToEmail("Forget the password", );
-                        fbAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                            @Override
-                            public void onComplete(@NonNull Task<AuthResult> task) {
-                                if (task.isSuccessful()) {
-                                    Toast.makeText(getApplicationContext(), "Saved", Toast.LENGTH_SHORT).show();
-                                }
-                                else {
-                                    Toast.makeText(getApplicationContext(), "ERROR", Toast.LENGTH_SHORT).show();
-                                }
-                            }
-                        });
-                    }
-                    else {
-                        Toast.makeText(this, "IF ERROR", Toast.LENGTH_SHORT).show();
-                    }
-                });
-    }
-
-    public void onClickSignIn() {
-        btnSignIn = (Button) findViewById(R.id.sign_in);
-        btnSignIn.setOnClickListener(
+        // Переход на вход
+        findViewById(R.id.sign_in).setOnClickListener(
                 v -> {
                     Intent intent = new Intent(this, Authorization.class);
                     startActivity(intent);
                 }
         );
+    }
+
+    private void onClickRegistration() {
+
+        String name = edNickname.getText().toString();
+        String email = edEmail.getText().toString();
+        String password = edPassword.getText().toString();
+        String passwordCheck = edPasswordCheck.getText().toString();
+
+        // Проверка на интернет-соединение
+        if (!isOnline()) {
+            Toast.makeText(getApplicationContext(),
+                    "No internet connection",Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        // Пустое поле имени
+        if (name.isEmpty()) {
+            edNickname.setError("Name is required");
+            edNickname.requestFocus();
+            return;
+        }
+
+        // Длина имени меньше 5
+        if (name.length() < 5) {
+            edNickname.setError("Minimum length of name should be 5");
+            edNickname.requestFocus();
+            return;
+        }
+
+        // Пустое поле email
+        if (email.isEmpty()) {
+            edEmail.setError("Email is required");
+            edEmail.requestFocus();
+            return;
+        }
+
+        // Некорректный ввод логина
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            edEmail.setError("Please enter a valid email");
+            edEmail.requestFocus();
+            return;
+        }
+
+        // Пустое поле пароля
+        if (password.isEmpty()) {
+            edPassword.setError("Password is required");
+            edPassword.requestFocus();
+            return;
+        }
+
+        // Некорректная длина пароля
+        if (password.length() < 6) {
+            edPassword.setError("Minimum length of password should be 6");
+            edPassword.requestFocus();
+            return;
+        }
+
+        // Пароли не совпадают
+        if (!password.equals(passwordCheck)) {
+             edPassword.setError("Passwords don't match");
+             edPasswordCheck.setError("Passwords don't match");
+             edPassword.requestFocus();
+             edPasswordCheck.requestFocus();
+             return;
+        }
+
+        fbAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(
+                task -> {
+                    if (task.isSuccessful() ) {
+                        // Пользователь зарегистрировался и вошел в аккаунт
+                        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                        startActivity(intent);
+                    }
+                    else {
+                        // Получение ошибок при регистрации
+                        Toast.makeText(getApplicationContext(),
+                                task.getException().getLocalizedMessage(), Toast.LENGTH_LONG).show();
+                    }
+                }
+        );
+
+    }
+
+    protected boolean isOnline() {
+        String cs = Context.CONNECTIVITY_SERVICE;
+        ConnectivityManager cm = (ConnectivityManager)
+                getSystemService(cs);
+        if (cm.getActiveNetworkInfo() == null) {
+            return false;
+        } else {
+            return true;
+        }
     }
 }

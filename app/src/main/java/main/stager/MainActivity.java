715eaa94af1a4a7b42393174c6a6b5863438fc1e
Dedicated407 <ilyaboy@list.ru>
2021-03-29 13:13:37 +0300

    + Log out of account

--- app/src/main/java/main/stager/MainActivity.java
+++ app/src/main/java/main/stager/MainActivity.java
@@ -1,12 +1,26 @@
 package main.stager;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AuthorizedOnlyActivity {
-
+    private Button btnLogOut;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnLogOut = (Button) findViewById(R.id.btn_log_out);
+
+        btnLogOut.setOnClickListener(
+                v -> {
+                    FirebaseAuth.getInstance().signOut();
+                    finish();
+                    Intent intent = new Intent(this, Authorization.class);
+                    startActivity(intent);
+                }
+        );
     }
 }

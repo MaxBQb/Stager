dc950a87f9aea9c5bc32ed010bc51bdac187370c
Dedicated407 <ilyaboy@list.ru>
2021-04-05 12:05:56 +0300

    Connected the sidebar to the app

--- app/src/main/java/main/stager/MainActivity.java
+++ app/src/main/java/main/stager/MainActivity.java
@@ -4,7 +4,18 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.google.android.material.navigation.NavigationView;
+
 public class MainActivity extends AuthorizedOnlyActivity {
+
+    private AppBarConfiguration mAppBarConfiguration;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,12 +23,33 @@ public class MainActivity extends AuthorizedOnlyActivity {
         setContentView(R.layout.activity_main);
         if (mAuth.getCurrentUser() == null) return;
 
-        ((Button) findViewById(R.id.btn_log_out)).setOnClickListener(
-                v -> {
-                    mAuth.signOut();
-                    finish();
-                    startActivity(new Intent(this, Authorization.class));
-                }
-        );
+//        ((Button) findViewById(R.id.btn_log_out)).setOnClickListener(
+//                v -> {
+//                    mAuth.signOut();
+//                    finish();
+//                    startActivity(new Intent(this, Authorization.class));
+//                }
+//        );
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.nav_about_me, R.id.nav_my_contacts, R.id.nav_my_actions)
+                .setDrawerLayout(drawer)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+        NavigationUI.setupWithNavController(navigationView, navController);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
+                || super.onSupportNavigateUp();
     }
 }

659d50d23d236eabe650dff8c769d953c0b8f1e5
Dedicated407 <ilyaboy@list.ru>
2021-04-12 12:45:14 +0300

    Implementation of the fragment about me

--- app/src/main/java/main/stager/ui/about_me/AboutMeFragment.java
+++ app/src/main/java/main/stager/ui/about_me/AboutMeFragment.java
@@ -1,28 +1,38 @@
 package main.stager.ui.about_me;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
 
+import com.google.firebase.auth.FirebaseAuth;
+
+import main.stager.MainActivity;
 import main.stager.R;
 
 public class AboutMeFragment extends Fragment {
 
-    private AboutMeViewModel aboutMeViewModel;
+    private FirebaseAuth fbAuth;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        aboutMeViewModel =
-                new ViewModelProvider(this).get(AboutMeViewModel.class);
+
         View root = inflater.inflate(R.layout.fragment_about_me, container, false);
-        final TextView textView = root.findViewById(R.id.text_about_me);
-        aboutMeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        root.findViewById(R.id.textView);
+        ((Button) root.findViewById(R.id.btn_exit_in_acc)).setOnClickListener(
+                v -> {
+                    fbAuth = FirebaseAuth.getInstance();
+                    startActivity(new Intent(this.getActivity(), MainActivity.class));
+                    fbAuth.signOut();
+
+                }
+        );
+
         return root;
     }
 }

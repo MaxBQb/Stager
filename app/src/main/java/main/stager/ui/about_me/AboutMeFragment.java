17287e41af0ee57f0330d1bea5ba45664761bfa2
Бабышев Максим <cmdazaza@gmail.com>
2021-04-19 16:24:04 +0300

    Refactor AboutMeFragment

--- app/src/main/java/main/stager/ui/about_me/AboutMeFragment.java
+++ app/src/main/java/main/stager/ui/about_me/AboutMeFragment.java
@@ -1,58 +1,62 @@
 package main.stager.ui.about_me;
 
 import android.content.Intent;
-import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
-import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 import android.widget.EditText;
-import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
-
 import main.stager.ChangeListeners.OnLostFocus;
 import main.stager.DataProvider;
 import main.stager.MainActivity;
 import main.stager.R;
+import main.stager.StagerVMFragment;
 
-public class AboutMeFragment extends Fragment {
-    private AboutMeViewModel viewModel;
+public class AboutMeFragment extends StagerVMFragment<AboutMeViewModel> {
+    private EditText inputName;
+    private EditText inputDescription;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        viewModel = new ViewModelProvider(this).get(AboutMeViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_about_me, container, false);
-        root.findViewById(R.id.btn_exit_in_acc).setOnClickListener(
-                v -> {
-                    FirebaseAuth.getInstance().signOut();
-                    startActivity(new Intent(this.getActivity(), MainActivity.class));
-                    this.getActivity().finish();
-                }
-        );
-        EditText input_name = root.findViewById(R.id.personName);
-        EditText input_description = root.findViewById(R.id.secondName);
-        viewModel.getText().observe(getViewLifecycleOwner(), input_name::setText);
-        viewModel.getDescription().observe(getViewLifecycleOwner(), input_description::setText);
+    @Override
+    protected Class<AboutMeViewModel> getViewModelType() {
+        return AboutMeViewModel.class;
+    }
 
-        input_name.setOnFocusChangeListener(new OnLostFocus() {
+    @Override
+    protected int getViewBaseLayoutId() {
+        return R.layout.fragment_about_me;
+    }
+
+    @Override
+    protected void setEventListeners() {
+        super.setEventListeners();
+        view.findViewById(R.id.btn_exit_in_acc).setOnClickListener(v -> {
+            FirebaseAuth.getInstance().signOut();
+            startActivity(new Intent(this.getActivity(), MainActivity.class));
+            this.getActivity().finish();
+        });
+        inputName.setOnFocusChangeListener(new OnLostFocus() {
             @Override
             public DatabaseReference getDataRef(DataProvider dp) {
-                return dp.getUserName();
+            return dp.getUserName();
             }
         });
-
-        input_description.setOnFocusChangeListener(new OnLostFocus() {
+        inputDescription.setOnFocusChangeListener(new OnLostFocus() {
             @Override
             public DatabaseReference getDataRef(DataProvider dp) {
-                return dp.getUserDescription();
+            return dp.getUserDescription();
             }
         });
+    }
 
-        return root;
+    @Override
+    protected void prepareFragmentComponents() {
+        super.prepareFragmentComponents();
+        inputName = view.findViewById(R.id.personName);
+        inputDescription = view.findViewById(R.id.secondName);
+    }
+
+    @Override
+    protected void setObservers() {
+        super.setObservers();
+        viewModel.getText().observe(getViewLifecycleOwner(), inputName::setText);
+        viewModel.getDescription().observe(getViewLifecycleOwner(), inputDescription::setText);
     }
 }

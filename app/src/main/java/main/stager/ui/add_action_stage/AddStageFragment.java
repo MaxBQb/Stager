f0fa5c1d23612970cbc8028ae6848c8378a10019
Бабышев Максим <cmdazaza@gmail.com>
2021-04-19 17:06:58 +0300

    Refactor AddStageFragment

--- app/src/main/java/main/stager/ui/add_action_stage/AddStageFragment.java
+++ app/src/main/java/main/stager/ui/add_action_stage/AddStageFragment.java
@@ -1,30 +1,24 @@
 package main.stager.ui.add_action_stage;
 
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
-import androidx.navigation.fragment.NavHostFragment;
 import main.stager.DataProvider;
 import main.stager.R;
+import main.stager.StagerFragment;
 import main.stager.model.Stage;
 import main.stager.model.Status;
 import main.stager.model.TriggerType;
 
-public class AddStageFragment extends Fragment {
+public class AddStageFragment extends StagerFragment {
     static public final String ARG_ACTION_KEY = "Stager.add_action_stage.param_action_key";
     private String mActionKey;
-    private AddStageViewModel mViewModel;
     private EditText inputName;
 
     @Override
@@ -37,14 +31,17 @@ public class AddStageFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
-                             @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_add_action_stage, container, false);
+    protected int getViewBaseLayoutId() {
+        return R.layout.fragment_add_action_stage;
+    }
+
+    @Override
+    protected void prepareFragmentComponents() {
+        super.prepareFragmentComponents();
         ((AppCompatActivity) getActivity())
                 .getSupportActionBar()
                 .setHomeAsUpIndicator(R.drawable.ic_close);
         inputName = view.findViewById(R.id.add_action_stage_input_name);
-        return view;
     }
 
     @Override
@@ -74,19 +71,6 @@ public class AddStageFragment extends Fragment {
         }
         DataProvider.getInstance().addStage(mActionKey,
                 new Stage(Status.WAITING, name, TriggerType.MANUAL));
-        go_back();
-    }
-
-    private void go_back() {
-        ((NavHostFragment) getActivity()
-                .getSupportFragmentManager()
-                .findFragmentById(R.id.nav_host_fragment))
-                .getNavController().navigateUp();
-    }
-
-    @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        mViewModel = new ViewModelProvider(this).get(AddStageViewModel.class);
+        navigator.navigateUp();
     }
 }
--- app/src/main/java/main/stager/ui/add_action_stage/AddStageViewModel.java
+++
@@ -1,7 +0,0 @@
-package main.stager.ui.add_action_stage;
-
-import androidx.lifecycle.ViewModel;
-
-public class AddStageViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
-}

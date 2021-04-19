4addce011a1891d4d1e453ef503bb448887f7a9b
Бабышев Максим <cmdazaza@gmail.com>
2021-04-19 20:35:50 +0300

    Refactor Authorization class

--- app/src/main/java/main/stager/Authorization.java
+++ app/src/main/java/main/stager/Authorization.java
@@ -5,15 +5,9 @@ import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.os.Bundle;
 import android.util.Patterns;
-import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
-import androidx.annotation.NonNull;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-
 import main.stager.Base.SmartActivity;
 
 public class Authorization extends SmartActivity {
@@ -45,70 +39,64 @@ public class Authorization extends SmartActivity {
         edPassword = findViewById(R.id.password);
 
         // Переход на регистрацию
-        findViewById(R.id.btn_registration).setOnClickListener(
-                v -> {
-                    Intent intent = new Intent(this, Registration.class);
-                    startActivity(intent);
-                }
-        );
-
-        signIn();
+        findViewById(R.id.btn_registration).setOnClickListener(v -> {
+            Intent intent = new Intent(this, Registration.class);
+            startActivity(intent);
+        });
+        findViewById(R.id.success_btn).setOnFocusChangeListener((v, hasFocus) -> {
+            if (hasFocus) signIn();
+        });
     }
 
     private void signIn() {
+
         // Вход в аккаунт
-        findViewById(R.id.success_btn).setOnFocusChangeListener(
-                (v, hasFocus) -> {
-                    String email = edEmail.getText().toString();
-                    String password = edPassword.getText().toString();
+        String email = edEmail.getText().toString();
+        String password = edPassword.getText().toString();
 
-                    // Проверка на интернет-соединение
-                    if (!isOnline()) {
-                        Toast.makeText(getApplicationContext(),
-                                "No internet connection",Toast.LENGTH_LONG).show();
-                        return;
-                    }
+        // Проверка на интернет-соединение
+        if (!isOnline()) {
+            Toast.makeText(getApplicationContext(),
+                    "No internet connection", Toast.LENGTH_LONG).show();
+            return;
+        }
 
-                    // Пустое поле email
-                    if (email.isEmpty()) {
-                        edEmail.setError("Email is required");
-                        edEmail.requestFocus();
-                        return;
-                    }
+        // Пустое поле email
+        if (email.isEmpty()) {
+            edEmail.setError("Email is required");
+            edEmail.requestFocus();
+            return;
+        }
 
-                    // Некорректный ввод логина
-                    if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
-                        edEmail.setError("Please enter a valid email");
-                        edEmail.requestFocus();
-                        return;
-                    }
+        // Некорректный ввод логина
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            edEmail.setError("Please enter a valid email");
+            edEmail.requestFocus();
+            return;
+        }
 
-                    // Пустое поле пароля
-                    if (password.isEmpty()) {
-                        edPassword.setError("Password is required");
-                        edPassword.requestFocus();
-                        return;
-                    }
+        // Пустое поле пароля
+        if (password.isEmpty()) {
+            edPassword.setError("Password is required");
+            edPassword.requestFocus();
+            return;
+        }
 
-                    // Некорректная длина пароля
-                    if (password.length() < 6) {
-                        edPassword.setError("Minimum length of password should be 6");
-                        edPassword.requestFocus();
-                        return;
-                    }
+        // Некорректная длина пароля
+        if (password.length() < 6) {
+            edPassword.setError("Minimum length of password should be 6");
+            edPassword.requestFocus();
+            return;
+        }
 
-                    fbAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this, task -> {
-                        if (task.isSuccessful()) {
-                            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                            startActivity(intent);
-                        } else {
-                            // Получение ошибок при входе
-                            Toast.makeText(getApplicationContext(),
-                                    task.getException().getLocalizedMessage(), Toast.LENGTH_LONG).show();
-                            }
-                        });
-                }
-        );
+        fbAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this, task -> {
+            if (task.isSuccessful()) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            } else if (task.getException() != null) // Получение ошибок при входе
+                Toast.makeText(getApplicationContext(),
+                        task.getException().getLocalizedMessage(), Toast.LENGTH_LONG).show();
+            });
     }
 
     protected boolean isOnline() {

3b436011d109730c233fc4fc3f1bcbf47fc51a2c
Бабышев Максим <cmdazaza@gmail.com>
2021-04-22 06:30:52 +0300

    + StagerExtendableList drag&drop events

--- app/src/main/java/main/stager/list/StagerExtendableList.java
+++ app/src/main/java/main/stager/list/StagerExtendableList.java
@@ -6,8 +6,9 @@ import androidx.annotation.NonNull;
 import androidx.lifecycle.ViewModel;
 import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.RecyclerView;
-
+import org.jetbrains.annotations.NotNull;
 import main.stager.R;
+import static com.google.android.material.color.MaterialColors.ALPHA_FULL;
 
 public abstract class StagerExtendableList<TVM extends ViewModel,
                                            TA extends StagerListAdapter<T,
@@ -17,25 +18,71 @@ public abstract class StagerExtendableList<TVM extends ViewModel,
     // Listeners
     protected abstract void onButtonAddClicked(View v);
 
-    protected void onItemMoved(int from, int to) {
+    protected void onItemDragged(int from, int to) {
         adapter.moveItem(from, to);
     }
 
+    protected void onItemDropped(int from, int to) {}
+
     protected void setEventListeners() {
         super.setEventListeners();
-        new ItemTouchHelper(new ItemTouchHelper.SimpleCallback(ItemTouchHelper.UP | ItemTouchHelper.DOWN |
-                ItemTouchHelper.START | ItemTouchHelper.END, 0) {
+        new ItemTouchHelper(new ItemDragAndDropCallback() {
             @Override
-            public boolean onMove(@NonNull RecyclerView recyclerView,
-                                  @NonNull RecyclerView.ViewHolder viewHolder,
-                                  @NonNull RecyclerView.ViewHolder target) {
-                onItemMoved(viewHolder.getAdapterPosition(), target.getAdapterPosition());
-                return false;
+            protected void onDrag(int from, int to) {
+                onItemDragged(from, to);
             }
 
             @Override
-            public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {}
+            protected void onDrop(int from, int to) {
+                onItemDropped(from, to);
+            }
         }).attachToRecyclerView(rv);
         view.findViewById(R.id.button_list_add).setOnClickListener(this::onButtonAddClicked);
+    }
+
+    protected abstract static class ItemDragAndDropCallback extends ItemTouchHelper.Callback {
+        private Integer mFrom = null;
+        private Integer mTo = null;
+
+        @Override
+        public int getMovementFlags(@NonNull RecyclerView recyclerView,
+                                    @NonNull RecyclerView.ViewHolder viewHolder) {
+            return makeMovementFlags(ItemTouchHelper.UP | ItemTouchHelper.DOWN, 0);
+        }
+
+        @Override
+        public boolean onMove(@NotNull RecyclerView recyclerView,
+                              RecyclerView.ViewHolder source,
+                              RecyclerView.ViewHolder target) {
+            if (source.getItemViewType() != target.getItemViewType())
+                return false;
+
+            if (mFrom == null)
+                mFrom = source.getAdapterPosition();
+            mTo = target.getAdapterPosition();
+
+            recyclerView.getAdapter().notifyItemMoved(source.getAdapterPosition(), target.getAdapterPosition());
+            onDrag(source.getAdapterPosition(), target.getAdapterPosition());
+            return true;
+        }
+
+        protected abstract void onDrag(int from, int to);
+        protected abstract void onDrop(int from, int to);
+
+        @Override
+        public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {}
+
+        @Override
+        public void clearView(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder) {
+            super.clearView(recyclerView, viewHolder);
+
+            viewHolder.itemView.setAlpha(ALPHA_FULL);
+
+            if (mFrom != null && mTo != null)
+                onDrop(mFrom, mTo);
+
+            // clear saved positions
+            mFrom = mTo = null;
+        }
     }
 }

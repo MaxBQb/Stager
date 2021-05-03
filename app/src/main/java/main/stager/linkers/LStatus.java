16fe9e0488cc527956f4bea4457fbb95100d00ab
Dedicated407 <ilyaboy@list.ru>
2021-05-03 22:38:51 +0300

    Refactor: makes file names more meaningful

--- app/src/main/java/main/stager/linkers/LStatus.java
+++ app/src/main/java/main/stager/linkers/LStatus.java
@@ -8,10 +8,10 @@ import main.stager.model.Status;
 public class LStatus {
     public static @DrawableRes int toIcon(@NotNull Status status) {
         switch (status) {
-            case ABORTED: return R.drawable.ic_stage_status_abort;
-            case SUCCEED: return R.drawable.ic_stage_status_succes;
-            case WAITING: return R.drawable.ic_stage_status_wait;
-            case LOCKED:  return R.drawable.ic_stage_status_locked;
+            case ABORTED: return R.drawable.ic_status_aborted;
+            case SUCCEED: return R.drawable.ic_status_succed;
+            case WAITING: return R.drawable.ic_status_waiting;
+            case LOCKED:  return R.drawable.ic_status_locked;
             default: throw new IllegalArgumentException();
         }
     }

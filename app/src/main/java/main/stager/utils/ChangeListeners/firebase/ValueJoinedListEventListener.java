03483e21236cb278170621990fce131957c17a02
Бабышев Максим <cmdazaza@gmail.com>
2021-05-06 13:21:08 +0300

    Refactor ValueJoinedListEventListener and optimize value tracing requests

--- app/src/main/java/main/stager/utils/ChangeListeners/firebase/ValueJoinedListEventListener.java
+++ app/src/main/java/main/stager/utils/ChangeListeners/firebase/ValueJoinedListEventListener.java
@@ -7,20 +7,19 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
-
 import org.jetbrains.annotations.NotNull;
-
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
-
 import main.stager.utils.DataProvider;
 
 public class ValueJoinedListEventListener<T> extends ValueListEventListener<T> {
     protected DatabaseReference source;
-    protected DatabaseReference keysSource;
-    protected List<T> tmpList;
+    protected Map<String, DatabaseReference> keySources = new HashMap<>();
+    protected List<T> tmpList = new ArrayList<>();
     protected Set<String> awaitedKeys = new HashSet<>();
     protected Set<String> listenKeys;
     protected ValueEventListener sourceListener = new SourceValueListener();
@@ -40,26 +39,33 @@ public class ValueJoinedListEventListener<T> extends ValueListEventListener<T> {
 
     @Override
     public void onDataChange(@NonNull DataSnapshot snapshot) {
-        keysSource = snapshot.getRef();
         if (!snapshot.exists()) {
             liveList.postValue(null);
             return;
         }
-        tmpList = new ArrayList<>();
-        awaitedKeys = new HashSet<>();
-        for (DataSnapshot postSnapshot: snapshot.getChildren())
-            awaitedKeys.add(postSnapshot.getKey());
 
-        if (listenKeys != null) {
-            for (String key: listenKeys)
-                source.child(key).removeEventListener(sourceListener);
+        tmpList.clear();
+        awaitedKeys.clear();
+        keySources.clear();
+
+        for (DataSnapshot postSnapshot: snapshot.getChildren()) {
+            String key = postSnapshot.getKey();
+            if (ignoredKeys.contains(key))
+                continue;
+            awaitedKeys.add(key);
+            keySources.put(key, postSnapshot.getRef());
         }
+
+        if (listenKeys != null)
+            for (String key: listenKeys)
+                removeSourceListener(key);
 
         listenKeys = new HashSet<>(awaitedKeys);
 
         for (String key: awaitedKeys)
-            source.child(key).addValueEventListener(sourceListener);
+            addSourceListener(key);
     }
+
 
     protected void onSourceDataChanged(@NotNull DataSnapshot snapshot) {
         if (!awaitedKeys.contains(snapshot.getKey()))
@@ -70,25 +76,45 @@ public class ValueJoinedListEventListener<T> extends ValueListEventListener<T> {
 
     protected void onExistingSourceDataChanged(@NotNull DataSnapshot snapshot) {
         // Just cause update of list of keys (joined to source)
-        if (snapshot.getKey() != null)
-            DataProvider.toggle(keysSource.child(snapshot.getKey()));
+
+        String key = snapshot.getKey();
+        if (key == null || !keySources.containsKey(key)) return;
+
+        DatabaseReference ref = keySources.get(key);
+        if (ref == null) return;
+
+        DataProvider.toggle(ref);
     }
 
     protected void onSourceDataGotInitial(@NotNull DataSnapshot snapshot) {
-        awaitedKeys.remove(snapshot.getKey());
-
-        T item;
-        if (snapshot.exists()) {
-            item = snapshot.getValue(className);
-            if (!exclude(item)) {
-                item = modify(item, snapshot);
-                if (!excludeModified(item))
-                    tmpList.add(item);
-            }
+        if (!snapshot.exists()) {
+            removeAwaited(snapshot.getKey());
+            return;
         }
 
+        T item = snapshot.getValue(className);
+        if (!isExcluded(item)) {
+            item = modify(item, snapshot);
+            if (!isModifiedExcluded(item))
+                tmpList.add(item);
+        }
+
+        removeAwaited(snapshot.getKey());
+    }
+
+    protected void removeAwaited(String key) {
+        awaitedKeys.remove(key);
         if (awaitedKeys.isEmpty())
             liveList.postValue(tmpList);
+    }
+
+
+    protected void removeSourceListener(@NonNull String key) {
+        source.child(key).removeEventListener(sourceListener);
+    }
+
+    protected void addSourceListener(@NonNull String key) {
+        source.child(key).addValueEventListener(sourceListener);
     }
 
     protected class SourceValueListener implements ValueEventListener {

48f5cc7ed1f07b462debbff91a38f5298d03b09d
Бабышев Максим <cmdazaza@gmail.com>
2021-05-19 19:09:54 +0300

    Customize received noty

--- app/src/main/java/main/stager/MyFirebaseMessagingService.java
+++ app/src/main/java/main/stager/MyFirebaseMessagingService.java
@@ -1,24 +1,38 @@
 package main.stager;
 
-import android.app.Notification;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
+import java.util.Random;
 
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
         super.onMessageReceived(remoteMessage);
-        Notification notification = new NotificationCompat.Builder(this,
+        final Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this , 0, intent,
+                PendingIntent.FLAG_ONE_SHOT);
+
+        Bitmap largeIcon = BitmapFactory.decodeResource(getResources(),
+                R.mipmap.ic_launcher_round);
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this,
                 getString(R.string.Notification__channel_id))
-                .setContentTitle(remoteMessage.getNotification().getTitle())
-                .setContentText(remoteMessage.getNotification().getBody())
+                .setContentTitle(remoteMessage.getData().get("title"))
+                .setContentText(remoteMessage.getData().get("body"))
                 .setSmallIcon(R.mipmap.ic_launcher)
+                .setLargeIcon(largeIcon)
                 .setAutoCancel(true)
-                .build();
+                .setContentIntent(pendingIntent);
+
         NotificationManagerCompat manager = NotificationManagerCompat.from(getApplicationContext());
-        manager.notify(635, notification);
+        int notificationID = new Random().nextInt(3000);
+        manager.notify(notificationID, notificationBuilder.build());
     }
 }

e1b12ce4d51f8dbbf05f104a7b13b6d1b175c177
Бабышев Максим <cmdazaza@gmail.com>
2021-05-26 08:58:49 +0300

    Refine flexibility & remove redundant field

--- app/src/main/java/main/stager/utils/BroadcasterHolders/BroadcastersHolder.java
+++ app/src/main/java/main/stager/utils/BroadcasterHolders/BroadcastersHolder.java
@@ -1,34 +1,41 @@
 package main.stager.utils.BroadcasterHolders;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 
 /* Class that allows to add event listeners to
 *  objects, which isn't created yet
 * */
 public class BroadcastersHolder<TB> {
-    protected Map<String, TB> broadcasters;
-    private Map<String, OnBroadcasterGained<TB>> onBroadcastersGained;
+    private Map<String, Set<OnBroadcasterGained<TB>>> onBroadcastersGainedListeners;
 
     BroadcastersHolder() {
         this(new HashMap<>());
     }
 
-    BroadcastersHolder(Map<String, OnBroadcasterGained<TB>> onBroadcastersGained) {
-        this.onBroadcastersGained = onBroadcastersGained;
-        broadcasters = new HashMap<>();
+    BroadcastersHolder(Map<String, Set<OnBroadcasterGained<TB>>> onBroadcastersGainedListeners) {
+        this.onBroadcastersGainedListeners = onBroadcastersGainedListeners;
     }
 
     public BroadcastersHolder<TB> addOnBroadcastGainListener(String key, OnBroadcasterGained<TB> listener) {
-        onBroadcastersGained.put(key, listener);
+        if (!onBroadcastersGainedListeners.containsKey(key))
+            onBroadcastersGainedListeners.put(key, new HashSet<>());
+        Set<OnBroadcasterGained<TB>> onBroadcasterGainedListeners =
+                                    onBroadcastersGainedListeners.get(key);
+        if (onBroadcasterGainedListeners != null)
+            onBroadcasterGainedListeners.add(listener);
         return this;
     }
 
     public void postBroadcaster(String key, TB broadcaster) {
-        broadcasters.put(key, broadcaster);
-        OnBroadcasterGained<TB> listener = onBroadcastersGained.get(key);
-        if (listener != null)
-            listener.addListeners(broadcaster);
+        Set<OnBroadcasterGained<TB>> onBroadcasterGainedListeners =
+                                    onBroadcastersGainedListeners.get(key);
+        if (onBroadcasterGainedListeners == null) return;
+        for (OnBroadcasterGained<TB> onBroadcasterGained : onBroadcasterGainedListeners)
+            if (onBroadcasterGained != null)
+                onBroadcasterGained.addListeners(broadcaster);
     }
 }
